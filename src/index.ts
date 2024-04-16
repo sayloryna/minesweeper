9a2b6839e558c29c8161174319ee766d73abe46a
Clara <clarafdezfraga@gmail.com>
2024-04-16 23:42:36 +0200

    Add borad type and create a 3x3 board with 1 bomb

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,27 @@ interface Tale {
   hasMine: boolean;
   totalNearMines: number;
 }
+
+interface Board {
+  row: Tale[];
+  column: Tale[][];
+}
+
+const tale: Tale = {
+  hasMine: false,
+  totalNearMines: 0,
+};
+const taleWithBomb = {
+  hasMine: true,
+  totalNearMines: 0,
+};
+
+const board: Board = {
+  row: [tale, tale, tale],
+  column: [
+    [tale, tale, tale],
+    [tale, taleWithBomb, tale],
+    [tale, tale, tale],
+  ],
+};
+console.log(board);
