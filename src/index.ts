039ff0f9231eb59c554117190f8b27e7a5563e0b
Clara <clarafdezfraga@gmail.com>
2024-04-18 00:19:15 +0200

    Add totalMines counter

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
 import { Tile, Board } from "./types";
 
 const createTile = (): Tile => {
-  let decider = Math.floor(Math.random() * 10);
+  let numberOutOfTen = Math.floor(Math.random() * 10);
   return {
-    hasMine: decider < 3,
+    hasMine: numberOutOfTen < 3,
     totalNearMines: 0,
   };
 };
 
 const board: Board = {
-  rows: 6,
-  columns: 6,
+  rows: 3,
+  columns: 3,
   boardDisplay: [
     [createTile(), createTile(), createTile()],
     [createTile(), createTile(), createTile()],
     [createTile(), createTile(), createTile()],
   ],
+};
+
+const searchTotalMines = (board: Board) => {
+  let totalMines = 0;
+  board.boardDisplay.forEach((row) => {
+    row.forEach((tile) => {
+      if (tile.hasMine === true) {
+        totalMines++;
+      }
+    });
+  });
+  return totalMines;
 };
