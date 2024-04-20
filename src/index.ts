88d7d3b6a5a9abb22493f32f0b430eb284418bc3
Clara <clarafdezfraga@gmail.com>
2024-04-20 21:04:18 +0200

    Add gameOver banner with restart game function

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ import { createBoard } from "./ui.js";
 import { playable6x6Board } from "./board.js";
 
 createBoard(playable6x6Board);
-
-document.querySelector(".board")?.appendChild(document.createElement("tr"));
--- src/ui.ts
+++ src/ui.ts
@@ -1,23 +1,31 @@
 import { playable6x6Board } from "./board.js";
 import { type Tile } from "./types.js";
+const boardContainer = document.querySelector(".board-container");
 
-const boardTable = document.querySelector(".board");
 const mainContent = document.querySelector(".main-content");
 const mainContainer = document.querySelector(".main-container");
 
 export const failedGame = () => {
   const gameOverBanner = document.createElement("div");
-  const gameOvertext = document.createElement("p");
+  const gameOverText = document.createElement("p");
+  const gameOverTitle = document.createElement("h2");
   gameOverBanner.classList.add("game-over");
 
-  gameOverBanner.innerText = "game over".toUpperCase();
-  gameOvertext.innerText =
+  gameOverTitle.innerText = "game over".toUpperCase();
+  gameOverText.innerText =
     "Has perdido, no has encontrado todas las bombas\n ¿Quieres probar otra vez?";
-  gameOverBanner.appendChild(gameOvertext);
+
+  gameOverBanner.appendChild(gameOverTitle);
+  gameOverBanner.appendChild(gameOverText);
 
   const restartButton = document.createElement("button");
+  restartButton.classList.add("button__restart");
   restartButton.innerText = "reiniciar".toUpperCase();
+
   restartButton.addEventListener("click", () => {
+    gameOverBanner.classList.add("not-show");
+    mainContent?.classList.remove("not-show");
+
     createBoard(playable6x6Board);
   });
 
@@ -28,25 +36,37 @@ export const failedGame = () => {
 };
 
 export const createBoard = (board: Tile[][]) => {
+  const boardTable = document.createElement("ul");
+  boardTable.classList.add("board");
   board.forEach((column) => {
-    const boardColumn = document.createElement("tr");
+    const boardColumn = document.createElement("div");
+
+    boardColumn.classList.add("column");
+
     column.forEach((row) => {
-      const cell = document.createElement("td");
-      cell.classList.add("tile", "hidden");
-      cell.addEventListener("click", () => {
-        cell.classList.remove("hidden");
+      const tile = document.createElement("li");
+
+      tile.classList.add("tile", "hidden");
+
+      tile.addEventListener("click", () => {
+        tile.classList.remove("hidden");
+
         if (row.hasMine) {
+          document.querySelector(".board")?.remove();
+
           failedGame();
         }
       });
 
-      boardColumn.appendChild(cell);
+      boardColumn.appendChild(tile);
       if (row.hasMine) {
-        cell.innerText = "💣";
+        tile.innerText = "💣";
       } else {
-        cell.innerText = row.totalNearMines.toString();
+        tile.innerText = row.totalNearMines.toString();
       }
     });
     boardTable?.appendChild(boardColumn);
   });
+
+  boardContainer?.appendChild(boardTable);
 };
