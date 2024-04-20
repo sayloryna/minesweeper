426f86c22081c0b3018f6b97842d9edfe9c97574
Clara <clarafdezfraga@gmail.com>
2024-04-20 18:20:22 +0200

    Add game over banner

--- src/ui.ts
+++ src/ui.ts
@@ -1,6 +1,31 @@
+import { playable6x6Board } from "./board.js";
 import { type Tile } from "./types.js";
 
 const boardTable = document.querySelector(".board");
+const mainContent = document.querySelector(".main-content");
+const mainContainer = document.querySelector(".main-container");
+
+export const failedGame = () => {
+  const gameOverBanner = document.createElement("div");
+  const gameOvertext = document.createElement("p");
+  gameOverBanner.classList.add("game-over");
+
+  gameOverBanner.innerText = "game over".toUpperCase();
+  gameOvertext.innerText =
+    "Has perdido, no has encontrado todas las bombas\n ¿Quieres probar otra vez?";
+  gameOverBanner.appendChild(gameOvertext);
+
+  const restartButton = document.createElement("button");
+  restartButton.innerText = "reiniciar".toUpperCase();
+  restartButton.addEventListener("click", () => {
+    createBoard(playable6x6Board);
+  });
+
+  gameOverBanner.appendChild(restartButton);
+
+  mainContent?.classList.add("not-show");
+  mainContainer?.appendChild(gameOverBanner);
+};
 
 export const createBoard = (board: Tile[][]) => {
   board.forEach((column) => {
@@ -10,6 +35,9 @@ export const createBoard = (board: Tile[][]) => {
       cell.classList.add("tile", "hidden");
       cell.addEventListener("click", () => {
         cell.classList.remove("hidden");
+        if (row.hasMine) {
+          failedGame();
+        }
       });
 
       boardColumn.appendChild(cell);
